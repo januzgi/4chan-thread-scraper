599768e8295c9a45b9aebdc9a95da807d789d1a3
Jani Suoranta <janisuoranta@icloud.com>
2020-04-21 15:02:32 +0300

    Fixes on Python since request cache couldn't be solved

--- bitcoinPrice.py
+++ bitcoinPrice.py
@@ -1,82 +1,32 @@
-import requests, json, sys
+import requests, json, sys, time
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
-from datetime import datetime
 
 
-def request_json():
-	print 'Begin request to get the json...'
-
-	# Try get request once
-	response = requests_retry_session().get('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
-	if (response.status_code == 200):
-		print 'Fetched price succesfully.\n'
-		return response.json()
-
-	# When above didn't succeed, retry 3 times using session 
-	s = requests.Session()
-	response = requests_retry_session(session=s).get(
-	    'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
-	)
-	# When requests succeeds using session
-	if (response.status_code == 200):
-		print 'Fetched price succesfully.\n'
-		return response.json()
-
-	print 'Couldn\'t fetch price json.'
-	return 'error'
-
-
-def requests_retry_session(
-    retries=3,
-    backoff_factor=0.3,
-    status_forcelist=(500, 502, 504),
-    session=None,
-):
-
-    session = session or requests.Session()
-    retry = Retry(
-        total=retries,
-        read=retries,
-        connect=retries,
-        backoff_factor=backoff_factor,
-        status_forcelist=status_forcelist,
-    )
-
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('https://', adapter)
-
-    return session
-
+def read_json():
+	fh = open('results/priceJson.txt', 'r')
+	jsonData = fh.read()
+	print 'Read priceJson.txt'
+	return jsonData
 
 
 
 def get_price_data(json):
-
 	price = str(json['bpi']['USD']['rate'])
 	# Strip the ',' from price, convert to float and to int
 	price = int(float(price.replace(',', '')))
-
 	return price
 
 
 
 
 def main():
-	# Send a request for the bitcoin price json
-	priceJson = request_json()
-	# Check if the request and retries failed
-	if (json == 'error'):
-		print 'Terminating bitcoinPrice.py script.'
-		sys.exit()
-
+	# Read BTC price from the json
+	priceJson = read_json()
 	# Get the data from the response json
-	priceInt = get_price_data(priceJson)
-
-	# Get timestamp
-	# Returns a datetime object containing the local time
-	dateTimeObj = datetime.now()
-	date = dateTimeObj.strftime("%d-%b-%Y %H:%M")
+	priceInt = get_price_data(json.loads(priceJson))
+	# Get timestamp as milliseconds
+	milli_sec = int(round(time.time() * 1000))
 	
 	# Read the colordata from colors.txt
 	# The format is: '63,61' where greenCount,redCount
@@ -85,12 +35,12 @@ def main():
 	gCount = colorData.split(',')[0]
 	rCount = colorData.split(',')[1]
 
-	# Create a string with the price and color data
-	dataString = "{\"timestamp\": %s, \"price\": %d, \"gCount\": %s, \"rCount\": %s}" % (str(date), priceInt, gCount, rCount)
+	# Create a string in json format with the price and color data
+	dataString = "{\"timestamp\": \"%d\", \"price\": \"%d\", \"gCount\": \"%s\", \"rCount\": \"%s\"}" % (milli_sec, priceInt, gCount, rCount)
 	print dataString
 
 	# Read and write to results.txt
-	fh = open('results.txt', 'a')
+	fh = open('results/results.txt', 'a')
 	fh.write(dataString + '\n')
 	fh.close()
 	print '\nSuccesfully saved BTC price and color data to results.txt'
